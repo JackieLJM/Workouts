8817ca75de88cdc6e0bbd6bb0b3f6b77ea89b313
JackieLJM <ljmlionsy@live.com>
2017-08-02 23:47:02 +0800

    leet11

---
+++ leetcode/11-Container-With-Most-Water.js
@@ -0,0 +1,34 @@
+// ES5 自己写的和解决方案超时的两种算法
+// var maxArea = function (height) {
+//     if (height.length > 1) {
+//         var area = [];
+//         var maxarea=0;
+//         for (var i = 0; i < height.length; i++) {
+//             for (var j = i + 1; j < height.length; j++) {
+//                 // var y = (height[i] <= height[j] ? height[i] : height[j]),
+//                 //     x = j - i;
+//                 // area.push(x * y);
+//                 maxarea = Math.max(maxarea, Math.min(height[i], height[j]) * (j - i));
+//             }
+//         }
+//         return maxarea;
+//         // return area;
+//         // return area.sort(function(a,b){return a-b}).pop();
+//     }
+// };
+
+// ES5 20170802 77%Beated
+var maxArea = function (height) {
+    var maxarea = 0, l = 0, r = height.length - 1;
+        while (l < r) {
+            maxarea = Math.max(maxarea, Math.min(height[l], height[r]) * (r - l));
+            if (height[l] < height[r]){
+                l++;
+            }
+            else{
+                r--;
+            }
+        }
+        return maxarea;
+};
+console.log(maxArea([1,2,3,4]));
