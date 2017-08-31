81117f7a72b884ebfb5b567c8947c020dcd579e8
JackieLJM <ljmlionshy@live.com>
2017-08-31 18:08:29 +0800

    leet2

--- leetcode/2-Add-Two-Numbers.js
+++ leetcode/2-Add-Two-Numbers.js
@@ -1,17 +1,68 @@
-// ES5
-var addTwoNumbers = function(l1, l2) {
-    var ls=[0];
-    for(var index=0;index<l1.lenght;index++){
-        ls[index]=ls[index]+((l1[index]+l2[index])%10);
-        if((l1[index]+l2[index])>10){
-            ls[index+1]=1;
-        }else{
-            ls[index+1]=0;
+// You are given two non-empty linked lists representing two non-negative integers. 
+// The digits are stored in reverse order and each of their nodes contain a single digit. 
+// Add the two numbers and return it as a linked list.
+// You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+// Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+// Output: 7 -> 0 -> 8
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+
+function ListNode(val){
+    this.val=val;
+    this.next=null;
+}
+var l1={
+    val:1,
+    next:{
+        val:2,
+        next:{
+            val:3,
+            next:null
         }
     }
-    var lsNode=ls.slice(0,ls.length);
+}
+var l2={
+    val:1,
+    next:{
+        val:8,
+        next:{
+            val:3,
+            next:null
+        }
+    }
+}
+
+var addTwoNumbers = function(l1, l2) {
+    var ls=new ListNode();
+    // var i=1;
+    while(l1.next){
+        if(l1.val+l2.val>=10){
+            ls.val=(l1.val+l2.val)%10+ls.val;
+            console.log(ls.val);
+            ls.next=new ListNode();
+            ls.next.val=1;
+        }
+        else if(l1.val+l2.val<10){
+            ls.val=l1.val+l2.val;
+            ls.next=new ListNode();
+        }
+        l1=l1.next;
+        l2=l2.next;
+        // console.log(i++);
+    }
     return ls;
 };
-
-var list1=[2,4,3],list2=[5,6,4];
-console.log(addTwoNumbers(list1,list2));
+console.log(addTwoNumbers(l1,l2));
+console.log(l1);
