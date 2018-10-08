540e1b3e25cdf6e5b5107e7a4416c7f42a171f12
Jackie Benjamin Liu <ljmlionshy@live.com>
2018-10-08 13:55:52 +0800

    Update 41-First-Missing-Positive.js
    finish 41.

--- leetcode/ES/41-First-Missing-Positive.js
+++ leetcode/ES/41-First-Missing-Positive.js
@@ -3,5 +3,11 @@
  * @return {number}
  */
 var firstMissingPositive = function(nums) {
-    
+    var sort=Array.from(nums);
+    nums.sort();
+    for(var i=0;i<sort.length;i++){
+        if(nums[0]===sort[i]){
+            return i;
+        }
+    }
 };
